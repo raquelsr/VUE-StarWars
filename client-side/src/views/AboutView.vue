e38f1f9ffca403329eac9d0722925efdc2d46c49
raquelsr <raquelserranor@hotmail.com>
2022-03-16 10:03:35 +0100

    Add vuetify and test it

--- client-side/src/views/AboutView.vue
+++ client-side/src/views/AboutView.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <v-progress-circular
+      :size="70"
+      :width="7"
+      color="purple"
+      indeterminate
+    ></v-progress-circular>
   </div>
 </template>
