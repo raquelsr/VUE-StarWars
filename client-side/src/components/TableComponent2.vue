67e5bb7e0e6a3e42c1003f42197242d509485e55
raquelsr <raquelserranor@hotmail.com>
2022-03-16 13:47:47 +0100

    Search in table

--- client-side/src/components/TableComponent2.vue
+++ client-side/src/components/TableComponent2.vue
@@ -1,16 +1,31 @@
 <template>
   <div id="app">
     <v-app id="inspire">
-      <v-data-table
-        :footer-props="footerProps"
-        :headers="headers"
-        :items="data"
-        :loading="loading"
-        :options.sync="options"
-        :server-items-length="serverItems"
-        @pagination="updatePage"
-      >
-      </v-data-table>
+      <v-card>
+        <v-card-title>
+          People
+          <v-spacer></v-spacer>
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+            @change="filterByName"
+          ></v-text-field>
+        </v-card-title>
+        <v-data-table
+          :footer-props="footerProps"
+          :headers="headers"
+          :items="data"
+          :loading="loading"
+          :options.sync="options"
+          :server-items-length="serverItems"
+          @pagination="updatePage"
+          :search="search"
+        >
+        </v-data-table>
+      </v-card>
     </v-app>
   </div>
 </template>
@@ -49,20 +64,25 @@ export default {
     async getData(numberPage = 1) {
       this.loading = true;
       try {
-        const res = await fetch(`${this.apiURL}/?page=${numberPage}`);
+        const res = await fetch(
+          `${this.apiURL}/?page=${numberPage}&search=${this.search}`
+        );
         const data = await res.json();
+        this.loading = false;
         this.data = data.results;
         this.serverItems = data.count;
-        this.loading = false;
       } catch (error) {
-        console.error(error);
         this.loading = false;
+        console.error(error);
       }
     },
     updatePage(pagination) {
       const { page } = pagination;
       this.pagination = pagination;
       this.getData(page);
+    },
+    filterByName() {
+      this.getData();
     },
   },
 };
